b1cd12399dcac36ef6a0491341e9ef8bbf3c59e1
Anurag nayak <68118269+Anurag-345@users.noreply.github.com>
2022-01-02 17:08:24 +0530

    initial commit

---
+++ portfolio/index.js
@@ -0,0 +1,14 @@
+const sidenav = document.querySelector('.sidenav');
+
+const ham = document.getElementById('hamBurger');
+
+ham.addEventListener('click', () => {
+   
+    sidenav.style.display = "initial";
+    
+});
+
+const cross = document.getElementById('sidenavCross');
+cross.addEventListener('click', () => {
+    sidenav.style.display = "none";
+})
