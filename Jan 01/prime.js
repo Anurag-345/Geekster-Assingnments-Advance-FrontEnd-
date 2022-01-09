77d2fa87ca89c4b7828c1d8d1c8dbe657341e3ab
Anurag nayak <68118269+Anurag-345@users.noreply.github.com>
2022-01-09 09:42:27 +0530

    initial commit

--- Jan 01/prime.js
+++ Jan 01/prime.js
@@ -2,13 +2,13 @@
 
         Array.prototype.containsPrime = () => {
             let count = 0;
-            for (let i = 0; i < a.length; i++) {
-                if(a[i] == 2){
+            for (let i = 0; i < Array.length; i++) {
+                if(Array[i] == 2){
                     return true;
                 }
                 else{
-                     for (let j = 2; j < a[i]; j++) {
-                        if (a[i] % j == 0) {
+                     for (let j = 2; j < Array[i]; j++) {
+                        if (Array[i] % j == 0) {
                             var res = 'the num is not prime';
                             break
                         }
