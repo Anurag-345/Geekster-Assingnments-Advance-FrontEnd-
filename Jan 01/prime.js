71f4b3176eba7ac130c9734ed97db85522204af5
Anurag nayak <68118269+Anurag-345@users.noreply.github.com>
2022-01-09 09:43:47 +0530

    Update prime.js

--- Jan 01/prime.js
+++ Jan 01/prime.js
@@ -1,38 +1,38 @@
 
 
-        Array.prototype.containsPrime = () => {
-            let count = 0;
-            for (let i = 0; i < Array.length; i++) {
-                if(Array[i] == 2){
-                    return true;
-                }
-                else{
-                     for (let j = 2; j < Array[i]; j++) {
-                        if (Array[i] % j == 0) {
-                            var res = 'the num is not prime';
-                            break
-                        }
-                        else {
-                            var res = "the num is prime";
-                            count += 1;
-                            break;
+         Array.prototype.containsPrime = function () {
 
-                        }
-                    }
-                }
+           let count = 0;
 
-            }
+           for (let i = 0; i < this.length; i++) {
 
-            if (count > 0) {
-                return true;
-            }
+             if (this[i] == 2) return true;
+             
+             else {
+               for (let j = 2; j < this[i]; j++) {
+
+                 if (this[i] % j == 0) {
+                   var res = "the num is not prime";
+                   break;
+                 }
+                  else {
+                   var res = "the num is prime";
+                   count += 1;
+                   break;
+                 }
+               }
+             }
+           }
+
+           if (count > 0) {
+             return true;
+           }
             else {
-                return false;
-            }
+             return false;
+           }
+         };
 
-        }
+         let a = [4, 4, 4, 4, 4, 1, 3];
 
-        let a = [4, 4, 4, 4, 4, 1,3];
-
-        console.log(a.containsPrime());
+         console.log(a.containsPrime());
 
